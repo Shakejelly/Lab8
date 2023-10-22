f9e3de134cf7c1c0e7e042e4f779c3c494c35d16
Shakejelly <fredrich93@gmail.com>
2023-10-22 20:34:58 +0200

    ..

---
+++ Lab8/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+        public Employee(int id, string name, string gender, int salary)
+        {
+            Id = id;
+            Name = name;
+            Gender = gender;
+            Salary = salary;
+        }
+        
+
+    } 
+
+
+
+}
--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Lab8
+﻿
+namespace Lab8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Employee empolyee1 = new Employee(4142, "Ragnar", "Male", 774124);
+            Employee empolyee2 = new Employee(3250, "Konrad Von Chilklan", "Female", 121314);
+            Employee empolyee3 = new Employee(8765, "Soufhiejah", "Female", 100000000);
+            Employee empolyee4 = new Employee(7126, "Kurt-Olof", "Male", 89217);
+            Employee empolyee5 = new Employee(8123, "Julius Petrus Angeruds", "Female", 871);
+
+            Stack<Employee> stack = new Stack<Employee>();
+
+            
+
+            stack.Push(empolyee1);
+            stack.Push(empolyee2);
+            stack.Push(empolyee3);
+            stack.Push(empolyee4);
+            stack.Push(empolyee5);
+           
+            foreach (Employee employee in stack) 
+            {
+                
+            }
+            
         }
     }
 }
